5d5129db00f41dddeade4e2bc45c4a641de1b4d2
caraldi <araldicm@Caitlin-M-Araldi.local>
2016-09-22 11:16:47 -0700

    Added class demo code

--- class-04-templates/lecture/demo-code/handlebars/scripts/neighborhood.js
+++ class-04-templates/lecture/demo-code/handlebars/scripts/neighborhood.js
@@ -7,7 +7,17 @@ function Neighborhood (opts) {
 };
 
 Neighborhood.prototype.toHtml = function() {
+  // Gets stored html content (script take with id neighborhood-template)
+  var source = $('#neighborhood-template').html();
+  // Takes source template string and turns into function stored on var template --> function takes Handlebars object to insert data into each expression in template
+  var template = Handlebars.compile(source);
+  // Create new object for each neighborhood object attribute in data set (3) to pass through
+  var testContext = {name: 'first expression'};
+  // 
+  var html = template(this);
 
+  // Get back rendered html
+  return html;
 };
 
 neighborhoodDataSet.forEach(function(neighborhoodObject) {
